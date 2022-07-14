a948023ec9a3b81d4ec8ec297e5f75dc3598cf4f
M_2171923 <eric.florence@cognizant.com>
2022-07-14 13:49:42 -0500

    cleanup

--- src/features/Focus.js
+++ src/features/Focus.js
@@ -7,11 +7,11 @@ import { spacing } from "../utils/sizes";
 
 export const Focus = ({addSubject}) => {
 
-    const [subject, setSubject] = React.useState(null);
+    const [subject, setSubject] = useState(null);
 
     return(
 
-        <View style={styles.container}>
+        <View>
             <View style={styles.inputContainer}>
                 <TextInput style={styles.textInput} onChangeText={setSubject} label="What would you like to focus on?"/>
                 <View style={styles.button}>
@@ -20,14 +20,9 @@ export const Focus = ({addSubject}) => {
             </View>
         </View>
     )
-
 }
 
 const styles = StyleSheet.create({
-
-    container: {
-        // flex: 0.1,
-    },
 
     textInput: {
         flex: 1,
